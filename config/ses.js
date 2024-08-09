07c76a89bb5c10ef56582d1b55c35dbe36bd2eac
Geer68 <germhidalgo@gmail.com>
2024-08-09 14:32:12 -0300

    aws error?

--- config/ses.js
+++ config/ses.js
@@ -3,9 +3,12 @@ import { fromEnv } from "@aws-sdk/credential-providers";
 
 const REGION = "us-east-2";
 
-const sesClient = new SESClient({
-  region: REGION,
-  credentials: fromEnv(),
+export const sesClient = new SESClient({
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 export { sesClient };
