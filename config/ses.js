09fbe6d709f648918c41f00d6d7306d0a527fb5e
Geer68 <germhidalgo@gmail.com>
2024-08-09 14:37:18 -0300

    ses.js

--- config/ses.js
+++ config/ses.js
@@ -4,11 +4,8 @@ import { fromEnv } from "@aws-sdk/credential-providers";
 const REGION = "us-east-2";
 
 const sesClient = new SESClient({
-  region: "us-east-2",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
+  region: REGION,
+  credentials: fromEnv(),
 });
 
 export { sesClient };
